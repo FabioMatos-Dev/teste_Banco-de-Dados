fb7b438fcf5ac26ca2267e7d962f73253cebb81a
Fabio de Matos <117763868+FabioMatos-Dev@users.noreply.github.com>
2023-10-18 17:24:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Teste de BD</title>
+</head>
+<body>
+    <header>
+        <h1>SITE PARA CADASTRO E RELATÓRIOS</h1>
+    </header>
+   
+    <div class="flex-container">
+        <div class="Cadastro">
+            <h2 style="color:blue;bold">CADASTRO</h2>
+            <button style="height:45px;border:3px solid blue;background-color:#FCF94F";>
+                <a href="pessoa.php"><b><h3>Cadastrar Pessoa</h3></b></a><br>
+            </button>
+            <button style="height:45px;border:3px solid blue;background-color:#FCF94F";>
+                <a href="produto.php"><b><h3>Cadastrar Produto</h3></b></a><br>
+            </button>
+        </div> 
+
+        <div class="Consulta">
+            <h2 style="color:red;bold">CONSULTA</h2>
+            <button style="height:45px;border:3px solid red;background-color:#B2FE66 ";>
+                <a href="consultaPessoa.php"><b><h3>Consultar Pessoa</h3></b></a><br>
+            </button>
+            <button style="height:45px;border:3px solid red;background-color:#B2FE66 ";>
+                <a href="consultaProduto.php"><b><h3>Consultar Produto</h3></b></a><br>
+            </button>
+        </div> 
+    </div>
+    <div class="social-icons">
+        <a href="https://youtube.com/c/FabioMatoschannel"><i class='bx bxl-youtube' ></i></a>
+        <a href="https://vm.tiktok.com/ZMNPTahKa/"><i class='bx bxl-tiktok'></i></a>
+        <a href="https://www.instagram.com/fabiomatos.instaspace/"><i class='bx bxl-instagram' ></i></a>
+        <a href="https://m.facebook.com/fabiomatos.facepage/?ref=bookmarks"><i class='bx bxl-facebook'></i></a>
+        <a href="https://www.linkedin.com/in/fabio-de-matos-03664243"><i class='bx bxl-linkedin' ></i></a>
+        <a href="https://github.com/FabioMatos-Dev"><i class='bx bxl-github' ></i></a>
+        <a href="https://linktr.ee/fabio_matos"><i class='bx bx-link' ></i></a> 
+    </div>  
+</body>
+</html>
